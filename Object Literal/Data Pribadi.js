7af450edfd2d461efd51113d064aacc31c6f12bb
Alvin <alvin.christian7@yahoo.com>
2019-03-13 16:58:10 +0700

    coba

---
+++ Object Literal/Data Pribadi.js
@@ -0,0 +1,52 @@
+var persons = [{
+    id: 1,
+    "first name": "Andy",
+    lastname: "Klouz",
+    email: "andy0@gmail.com",
+    gender: "Male",
+    age: 33,
+    score: 80
+  }, {
+    id: 2,
+    "first name": "Refi",
+    last_name: "Castagne",
+    email: "refi@gmail.com",
+    gender: "Male",
+    age: 22,
+    score: 75
+  }, {
+    id: 3,
+    "first name": "Irvy",
+    last_name: "Florence",
+    email: "irvy@gmail.com",
+    gender: "Male",
+    age: 24,
+    score: 78
+  }, {
+    id: 4,
+    "first name": "Karin",
+    last_name: "Petkens",
+    email: "karin@gmail.com",
+    gender: "Female",
+    age: 25,
+    score: 98
+  }, {
+    id: 5,
+    "first name": "Shadow",
+    last_name: "Manuely",
+    email: "shadow@gmail.com",
+    gender: "Male",
+    age: 21,
+    score: 76
+  },
+  {
+    id: 6,
+    "first name": "Anita",
+    last_name: "Manuely",
+    email: "anita@gmail.com",
+    gender: "MaleFemale",
+    age: 21,
+    score: 76
+  },
+  ]
+  console.log(persons[1]['email']);  
