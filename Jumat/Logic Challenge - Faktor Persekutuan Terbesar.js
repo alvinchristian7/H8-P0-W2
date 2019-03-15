2ee6d5bae069cd28a8ae7a0791e3696a9ca367d1
Alvin <alvin.christian7@yahoo.com>
2019-03-15 11:15:00 +0700

    revisi

--- Jumat/Logic Challenge - Faktor Persekutuan Terbesar.js
+++ Jumat/Logic Challenge - Faktor Persekutuan Terbesar.js
@@ -1,20 +1,25 @@
 function fpb(angka1, angka2) {
     // you can only write your code here!
-    var a = [];
-    var b = [];
-    for(var i = 1; i <= angka1; i++) {
-        if(angka1 % i == 0) a.push(i)
+    // var a = [];
+    // var b = [];
+    // for(var i = 1; i <= angka1; i++) {
+    //     if(angka1 % i == 0) a.push(i)
+    // }
+    // for(var i = 1; i <= angka2; i++) {
+    //     if(angka2 % i == 0) b.push(i)
+    // }
+    // // console.log("A :",a,"\nB :",b);
+    // for(var i = a.length - 1; i >= 0; i--) {
+    //     for(var j = b.length - 1; j >= 0; j--) {
+    //         if(a[i] == b[j]) return a[i];
+    //     }
+    // }
+    // return "Gak ada bang";
+    var hasil = 0;
+    for(var i = 1; i <= angka1 ;i++) {
+        if(angka1 % i === 0 && angka2 % i === 0) hasil = i;
     }
-    for(var i = 1; i <= angka2; i++) {
-        if(angka2 % i == 0) b.push(i)
-    }
-    // console.log("A :",a,"\nB :",b);
-    for(var i = a.length - 1; i >= 0; i--) {
-        for(var j = b.length - 1; j >= 0; j--) {
-            if(a[i] == b[j]) return a[i];
-        }
-    }
-    return "Gak ada bang";
+    return hasil;
   }
   
   // TEST CASES
--- Jumat/Logic Challenge - Mengelompokkan Hewan.js
+++ Jumat/Logic Challenge - Mengelompokkan Hewan.js
@@ -1,3 +1,9 @@
+function majuin(animals, i) {
+    for(var j = i; j < animals.length - 1; j++) {
+        animals[j] = animals[j+1];
+    }
+    animals.length -= 1;
+}
 function groupAnimals(animals) {
     // you can only write your code here!
     var baru = [];
@@ -5,7 +11,7 @@ function groupAnimals(animals) {
     while (!done) {
         done = true;
         for (var i = 1; i < animals.length; i += 1) {
-        if (animals[i - 1] > animals[i]) {
+        if (animals[i - 1][0] > animals[i][0]) {
             done = false;
             var tmp = animals[i - 1];
             animals[i - 1] = animals[i];
@@ -13,18 +19,25 @@ function groupAnimals(animals) {
             }
         }
     }
-    var b = 0;
+    var a = 0;
+    var b;
     while(true) {
         if(animals.length == 0) break;
-        baru[b] = [animals[0]];
+        baru[a] = [animals[0]];
+        b = 1;
         for(var i = 1; i < animals.length; i++) {
-            if (animals[i][0] == animals[0][0]) {
-                baru[b].push(animals[i]);
-                animals.splice(i,1);
+            if (animals[i][0] == baru[a][b-1][0]) {
+                // baru[a].push(animals[i]);
+                // animals.splice(i,1);
+                baru[a][b] = animals[i];
+                majuin(animals, i);
+                // console.log(baru[a], animals, animals.length);
+                b++;
             }
         }
-        animals.splice(0,1);
-        b++;
+        majuin(animals, 0);
+        // animals.splice(0,1);
+        a++;
     }
     return baru;
 }
